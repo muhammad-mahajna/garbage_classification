769db9d6ff201505d7485b10603cf0579fd0c5db
Muhammad_Mahajna <muhammad.mahajna@ucalgary.ca>
2024-10-10 08:21:09 -0600

    data dir in talc

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:30:00
 #SBATCH --mem=4GB
 #SBATCH --job-name=MyFirstJobOnARC
-#SBATCH --partition=cpu
+#SBATCH --partition=cpu12
 #SBATCH --mail-type=END
 #SBATCH --mail-user=muhammad.mahajna@ucalgary.ca
 #SBATCH --output=OutputFromMyFirstJob_%j.out
