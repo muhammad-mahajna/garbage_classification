0eed21c2bf79da573bfc4a2ea78e89c22da1da20
Muhammad_Mahajna <muhammad.mahajna@ucalgary.ca>
2024-10-19 18:59:53 -0600

    updating env config

--- setup_conda_environment.sh
+++ setup_conda_environment.sh
@@ -6,7 +6,7 @@ if conda info --envs | grep -q "mm_enel645_assg2"; then
 else
   # Create the conda environment
   echo "Creating environment 'mm_enel645_assg2'..."
-  conda create -n mm_enel645_assg2 python=3.8 -y
+  conda create -n mm_enel645_assg2 python=3.12 -y
 fi
 
 # Activate the conda environment
@@ -15,11 +15,15 @@ source activate mm_enel645_assg2
 
 # Install the required packages
 echo "Installing required packages..."
+
+conda install -c conda-forge liblapack libblas -y
 conda install pytorch torchvision torchaudio -c pytorch -y
 conda install -c conda-forge transformers -y
 conda install -c conda-forge scikit-learn matplotlib seaborn -y
-conda install pillow numpy -y
+conda install pillow numpy pandas -y
 
 # List installed packages
 echo "Listing installed packages..."
 conda list
+
+conda env export > mm_enel645_assg2_environment.yml
